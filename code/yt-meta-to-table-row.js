f68c15b42a6eab607af328397f021ab2c1ef9466
raisreis <133985789+raisreis@users.noreply.github.com>
2024-06-22 12:30:21 +0200

    Fix incorrect video publish fetch when running in playlist

--- code/yt-meta-to-table-row.js
+++ code/yt-meta-to-table-row.js
@@ -1,6 +1,6 @@
 javascript: (() => {
   const title = document.querySelectorAll('[id="title"].style-scope.ytd-watch-metadata h1 yt-formatted-string')[0].innerHTML;
-  const d = new Date(document.querySelectorAll('meta[itemprop="datePublished"]')[0]['content']);
+  const d = new Date(document.getElementById('info-strings').querySelectorAll('yt-formatted-string.ytd-video-primary-info-renderer')[0].textContent);
   const pd = String(d.getFullYear()) + String(d.getMonth() + 1).padStart(2, '0') + String(d.getDate()).padStart(2, '0');
 
   const videoId = document.getElementById('page-manager').querySelectorAll('ytd-watch-metadata[video-id]')[0].getAttribute('video-id');
