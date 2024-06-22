4d1f30d33b9908f5c5569aac9f3426e278d6836c
raisreis <133985789+raisreis@users.noreply.github.com>
2024-06-22 09:30:04 +0200

    Fix incorrect video-ID fetch when using playlists

--- code/yt-meta-to-table-row.js
+++ code/yt-meta-to-table-row.js
@@ -2,8 +2,9 @@ javascript: (() => {
   const title = document.querySelectorAll('[id="title"].style-scope.ytd-watch-metadata h1 yt-formatted-string')[0].innerHTML;
   const d = new Date(document.querySelectorAll('meta[itemprop="datePublished"]')[0]['content']);
   const pd = String(d.getFullYear()) + String(d.getMonth() + 1).padStart(2, '0') + String(d.getDate()).padStart(2, '0');
-  const link = document.querySelectorAll('link[rel="canonical"]')[0].getAttribute('href');
 
+  const videoId = document.getElementById('page-manager').querySelectorAll('ytd-watch-metadata[video-id]')[0].getAttribute('video-id');
+  const link = "https://www.youtube.com/watch?v=" + videoId;
 
   const result = [link, title, pd].join('\t');
   window.prompt('', result);
